8c42329a48bb46780e3e6846875845a1ccad4fe3
codingmr <codingmroberts@gmail.com>
2017-02-13 15:17:51 +0000

    Update popup.js

--- popup.js
+++ popup.js
@@ -2,8 +2,8 @@
  * Initially called when extension page loads. Attaches click handlers to all the buttons.
  */
 function init() {
-	document.getElementById("pomodoro").innerText = localStorage["pomodoro-selection"] || 10;
-	document.getElementById("break").innerText = localStorage["break-selection"] || 10;
+	document.getElementById("pomodoro").innerText = localStorage["pomodoro-selection"] || 15;
+	document.getElementById("break").innerText = localStorage["break-selection"] || 15;
 
 	var buttonGroups = document.getElementsByClassName("time-buttons-group")
 	Array.prototype.forEach.call(buttonGroups, function(divElem) {
@@ -21,7 +21,7 @@ function init() {
  */
 function timeButtonOnClickHandler(event) {
 	var targetElem = event.target;
-	var timeSelected = +targetElem.innerText; // Get button text and convert to number
+	var timeSelected = 15; // Get button text and convert to number
 	var settingKey = targetElem.parentNode.id; // Get id of node (tells us whether break or pomodoro)
 	localStorage[settingKey] = timeSelected; // Save in localstorage.
 	document.getElementById(settingKey.split("-")[0]).innerText = timeSelected;
