f370b85a97be4a8ea8321286add5e518b41aba2c
codingmr <codingmroberts@gmail.com>
2017-02-20 16:46:26 +0000

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,26 +1,35 @@
 // inherits background.js
 var bgpage = chrome.extension.getBackgroundPage();
+var totalSeconds = 0;
+var playing = false;
+var varTimer;
 
 // primary function to connect to extension
 document.addEventListener('DOMContentLoaded', function () {
 
 	// listens for a mouse click
 	// document.querySelector(connect to html id).addEventListener('type of event', which function to call if event happens);
-    document.querySelector('#plusFive').addEventListener('click', plusMe);
-	document.querySelector('#minusFive').addEventListener('click', minusMe);
+    document.querySelector('#start').addEventListener('click', start);
+	document.querySelector('#stopPLEASE').addEventListener('click', stopPLEASE);
 	
 });
 
-function plusMe()
+function start()
 {
-	// lol okay, if you try += 5 it adds 5 as a string to it
-	// but if you try -= it minus as an integer
-	// so this fix works -= -
-	document.getElementById("numberLabel").textContent -= -5;
+	if (varTimer)
+	{
+		clearInterval(varTimer);
+	}
+	varTimer = setInterval(increment, 1000);
 }
-	//If you make it +document.getElementById("numberLabel").textContent += 5 it adds as an integer
 
-function minusMe()
+function stopPLEASE()
 {
-	document.getElementById("numberLabel").textContent -= 5;
+	clearInterval(varTimer);
+}
+
+function increment()
+{
+	 ++totalSeconds;
+	 document.getElementById("numberLabel").textContent = totalSeconds%60;
 }
