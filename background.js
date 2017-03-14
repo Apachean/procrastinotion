3c9d0d1d33a3662b228eb6738323ab9c60055430
codingmr <codingmroberts@gmail.com>
2017-03-14 12:15:32 +0000

    Add files via upload

--- background.js
+++ background.js
@@ -1,5 +1,122 @@
-<<<<<<< HEAD
-// timer code would go here
-=======
-// timer code would go here
->>>>>>> origin/Matt
+var totalSeconds = 33;
+var workingTime = 0;
+var surfingTime = 0;
+var timerState = 1;
+
+// make the array work in checkURL(url)
+var arrayOfSurfSites = ["facebook", "youtube"];
+
+var varTimer;
+
+// Fires when tab is changed
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	chrome.tabs.get(activeInfo.tabId, function (tab) {
+		if (timerState==1) checkURL(tab.url);		
+	});
+});
+
+// Fires when url is changed
+chrome.tabs.onUpdated.addListener(function(tabId, props) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		if (timerState==1) checkURL(tabs[0].url);
+	});
+});
+
+// Fires when chrome loads -- gets first active page
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	if (timerState==1) checkURL(tabs[0].url);
+});
+
+function resumeTimer()
+{
+	timerState = 1;
+	
+	// no idea but it made it work soooooooooooo
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		checkURL(tabs[0].url);
+	});
+}
+
+function pauseTimer()
+{
+	clearInterval(varTimer);
+	timerState = 0;
+}
+
+
+function resumeWorkTime()
+{
+	if (varTimer)
+	{
+		clearInterval(varTimer);
+	}
+	varTimer = setInterval(incrementWork, 1000);
+}
+
+function pauseWorkTime()
+{
+	clearInterval(varTimer);
+}
+
+function incrementWork()
+{
+	++workingTime;
+	chrome.browserAction.setBadgeText({text: String(workingTime)});
+}
+
+function resumeSurfTime()
+{
+	if (varTimer)
+	{
+		clearInterval(varTimer);
+	}
+	varTimer = setInterval(incrementSurf, 1000);
+}
+
+function pauseSurfTime()
+{
+	clearInterval(varTimer);
+}
+
+function incrementSurf()
+{
+	++surfingTime;
+	chrome.browserAction.setBadgeText({text: String(surfingTime)});
+}
+
+//The function below checks if the URL includes a bad string (This is seemingly the best way)
+function checkURL(url) 
+{
+	if (url.includes("reddit") || url.includes("facebook") || url.includes("youtube"))
+	{
+		chrome.browserAction.setBadgeBackgroundColor({color:[254, 84, 66, 1]}); // red
+		//chrome.browserAction.setBadgeText({text: "Bad"});
+		chrome.browserAction.setBadgeText({text: String(surfingTime)});
+		
+		pauseWorkTime();
+		resumeSurfTime();
+	}
+	else 
+	{
+		chrome.browserAction.setBadgeBackgroundColor({color:[0, 172, 140, 1]}); // green
+		//chrome.browserAction.setBadgeText({text: "Good"});
+		chrome.browserAction.setBadgeText({text: String(workingTime)});
+		
+		pauseSurfTime();
+		resumeWorkTime();
+	}
+}
+
+function getWorkTime()
+{
+	return workingTime;
+}
+
+function getSurfTime()
+{
+	return surfingTime;
+}
+
+
+
+
